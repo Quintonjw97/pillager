a79b07d8ca89d566768283debdbdeb50188d6b2d
Quinton J. Williams <qscorp97@gmail.com>
2025-03-04 15:29:44 -0800

    Tweak 4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../../src'))  # Source code dir relative to this file
+
+
 project = 'pillager'
 copyright = '2025, Quinton J. Williams'
 author = 'Quinton J. Williams'

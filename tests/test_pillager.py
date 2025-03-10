4fc157ceefe3fb01f4cbbf38c6833098c7ed0275
Quinton J. Williams <qscorp97@gmail.com>
2025-03-09 21:50:50 -0700

    Update to tests.

--- tests/test_pillager.py
+++ tests/test_pillager.py
@@ -1,25 +1,45 @@
 import pillager.pillager as pg
-import pytest
+import pytest as pt
+import numpy as np
+import os
 
 t_class = pg.Pillager()
-
+t_class.output_dir= os.path.dirname(__file__) + '/util_data/'
 def test_blacklist_dir_files(obj=t_class):
-    assert True
+    obj.blacklist_dir_files()
+    assert [obj.input_base,obj.control_base,'serpent.i.wrk','outputs.csv','serpent.i_det0.m','serpent.i_res.m'] ==  obj.file_blacklist
 
 def test_run_search(obj=t_class):
     assert True
 
 def test_regressive_secant(obj=t_class):
-    assert True
+    ans = np.exp(0)
+    y = [-1,1]
+    f = [ans - np.exp(y[0]),ans - np.exp(y[1])]
+    for i in range(5):
+        val = obj.regressive_secant(y,f)
+        y.append(val)
+        f.append(ans - np.exp(y[-1]))
+    assert pt.approx(np.exp(y[-1])) == ans
 
 def test_generalized_regressive_secant(obj=t_class):
-    assert True
+    ans = np.exp(0)
+    y = [-1,1]
+    sigs = [0.0001,0.0001]
+    f = [ans - np.exp(y[0]),ans - np.exp(y[1])]
+    for i in range(6):
+        val = obj.generalized_regressive_secant(y,f,sigs)
+        y.append(val)
+        f.append(ans - np.exp(y[-1]))
+        sigs.append(0.001)
+    assert pt.approx(np.exp(y[-1])) == ans
 
 def test_get_eigenvalue(obj=t_class):
-    assert True
+    assert obj.get_eigenvalue() == ('9.99716E-01','0.00021')
 
 def test_get_detector_values(obj=t_class):
-    assert True
+    df = obj.get_detector_values('serpent.i_det0.m')
+    assert df.shape == (1,516)
 
 def test_write_serpent(obj=t_class):
     assert True
